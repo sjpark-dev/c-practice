e861a1f44699c6cd544ed407645b8881e07d3d64
sjpark-dev <sjpark.dev@gmail.com>
2020-12-21 13:20:38 +0900

    add example01.c

---
+++ section00/example01.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main() {
+	printf("@@@@@\n");
+	printf("@@@@@\n");
+	printf("@@@@@\n\n");
+	
+	printf("@@@@@\n");
+	printf("@   @\n");
+	printf("@   @\n");
+	printf("@@@@@\n\n");
+	
+	printf("      $      \n");
+	printf("    $ $ $    \n");
+	printf("  $ $ $ $ $  \n");
+	printf("$ $ $ $ $ $ $\n\n");
+	
+	printf("BBBBBB\n");
+	printf("B     B\n");
+	printf("B     B\n");
+	printf("BBBBBB\n");
+	printf("B     B\n");
+	printf("B     B\n");
+	printf("BBBBBB\n");
+	
+	return 0;
+}
+
